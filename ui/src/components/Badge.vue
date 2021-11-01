9c1b4f96a8030b659d62fb832d7477f9326642ba
Kaichao Sun <kaichaosuna@gmail.com>
2021-11-01 16:55:10 +0800

    kanban taskcard view

---
+++ ui/src/components/Badge.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="px-3 h-6 rounded-full text-xs font-semibold flex items-center"
+    :class="`bg-${color}-100 text-${color}-700`"
+  >
+    <span class="w-2 h-2 rounded-full mr-1" :class="`bg-${color}-400`"></span>
+    <span>
+      <slot></slot>
+    </span>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    color: {
+      type: String,
+      default: "teal"
+    }
+  }
+});
+</script>
